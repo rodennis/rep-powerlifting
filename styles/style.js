558b7784ae57934b5ec12f6ea46390d326b2da14
rodennis <rev.rltodd@gmail.com>
2021-09-28 18:23:31 -0500

    set the signup field to clear and close at onset of submiting the signup fields.

--- styles/style.js
+++ styles/style.js
@@ -2,7 +2,7 @@ const button = document.querySelector("button");
 const signUp = document.querySelector("#signUp");
 const signUpSubmit = document.querySelector("#signUpSubmit");
 
-
+/**getting the values of the login input fields and returning them to the cnsole*/
 button.addEventListener('click', function getInput() {
     const userName = document.getElementById("userName").value; 
     const passWord = document.getElementById("passWord").value;
@@ -17,12 +17,13 @@ button.addEventListener('click', function getInput() {
     }
 })
 
-
+/**calling the pop up window for the sign up screen */
 signUp.addEventListener('click', function onButtonClick() {
     document.getElementById('signUpForm').className="show";
 
 })
 
+/**calling all of the values of input fields, and returning them to the console, and closing the window afterwards */
 signUpSubmit.addEventListener('click', function register() {
     const first = document.getElementById("firstName").value;
     const last = document.getElementById("lastName").value;
@@ -41,6 +42,13 @@ signUpSubmit.addEventListener('click', function register() {
         "Email:" + " " + email + "\n" + "Password:" + " " + pW + "\n" + "Sex:" + " " + male + 
         "\n" + "Sex:" + " " + female)
 
+        document.getElementById("firstName").value='';
+        document.getElementById("lastName").value='';
+        document.getElementById("email").value='';
+        document.getElementById("pW").value='';
+        document.getElementById("male").value='';
+        document.getElementById("female").value='';
         document.getElementById('signUpForm').className="hide";
+
     }
 })
